e1601ef9c491ced4da3a30f9d3543efd65fc82e8
KirillKovardakov <kir.kovardakov@gmail.com>
2025-06-11 01:02:51 +0400

    Added two functions of filtering dictionaries in Processing.py and implemented a function in Main.py, which is responsible for the main logic of the project with the user and connects functionality between themselves. Also tests

--- src/processing.py
+++ src/processing.py
@@ -1,49 +1,17 @@
-import re
-from collections import Counter
-from src.utils import load_transactions
+from typing import Union
 
 
 def filter_by_state(list_dicts: list, state: str = 'EXECUTED') -> list:
     """Возвращает список, отфильтрованный по состоянию (state)"""
-    if len(list_dicts) == 0 or not isinstance(state, str):
+    if len(list_dicts) == 0 or state not in [x.get('state') for x in list_dicts] or not isinstance(state, str):
         raise ValueError
-    filtered_by_state = []
-    for transaction in list_dicts:
-        if 'state' in transaction and isinstance(transaction.get("state"), str):
-            if transaction.get('state').lower() == state.lower():
-                filtered_by_state.append(transaction)
+    filtered_by_state = [dictionary for dictionary in list_dicts if dictionary.get('state') == state]
     return filtered_by_state
 
 
-def sort_by_date(list_dicts: list, is_increase: bool = True) -> list:
-    """Сортирует список по дате(date). По умолчанию - по убыванию"""
-    if len(list_dicts) == 0 or not isinstance(is_increase, bool):
+def sort_by_date(list_dicts: list, reversed: bool = True) -> list:
+    """Сортирует список по дате(date)"""
+    if len(list_dicts) == 0 or not isinstance(reversed, bool):
         raise ValueError
-    sorted_list_by_date = sorted(list_dicts, key=lambda x: x.get('date'), reverse=is_increase)
+    sorted_list_by_date = sorted(list_dicts, key=lambda x: x.get('date'), reverse=reversed)
     return sorted_list_by_date
-
-
-def filter_by_state_re(transactions: list, state: str) -> list:
-    """Возвращает список словарей, у которых в описании есть данная строка"""
-    search_pattern = re.compile(re.escape(state), re.IGNORECASE)
-    result = []
-    for transaction in transactions:
-        if 'description' in transaction and isinstance(transaction.get("description"), str):
-            if search_pattern.search(transaction.get("description", "")):
-                result.append(transaction)
-    return result
-
-
-def filter_by_description(transactions: list, categories: list) -> dict:
-    """Возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
-
-    filtered_transactions = []
-    for transaction in transactions:
-        description = transaction.get('description', '')
-
-        for category in categories:
-            if re.search(category, description, re.IGNORECASE):
-                filtered_transactions.append(description)
-                break
-    category_counter = Counter(filtered_transactions)
-    return dict(category_counter)
